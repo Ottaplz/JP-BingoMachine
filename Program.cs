53e2fed32fa0d79b8730d042f7944c2c31f73472
David Murray <dcmurray13@hotmail.com>
2024-12-20 14:34:18 +1030

    First commit: The structure of taking user input is fully complete

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿string input = "";
+int MIN = 1;
+int MAX = 75;
+bool success = false;
+
+// Receive input from user for default or custom number range
+ProgramWelcome(success);
+
+if (input == "Y" || input == "YES")
+{   
+    MIN = InputMin(success);
+    MAX = InputMax(success, MIN);
+}
+
+Console.WriteLine($"Range: {MIN} to {MAX}");
+
+// Initialise and populate Array from MIN to MAX values desired
+CreateBingoBalls(MIN, MAX);
+
+
+// Function Definitions
+
+void ProgramWelcome(bool success)
+{
+    while (!success)
+        {
+            Console.WriteLine("Would you like to set a custom range of numbers? (Y/N)");
+            input = Console.ReadLine().ToString().ToUpper();
+            if (input == "Y" || input == "YES" || input == "N" || input == "NO")
+            {
+                success = true;
+            }
+        }
+}
+
+int InputMin(bool success)
+{
+    string input = "";
+    int value = 0;
+
+    // Ensure conditions of correct user input
+    while (success != true && value < 10000)
+    {
+        Console.WriteLine("Enter the minimum value: ");
+        input = (Console.ReadLine());
+        success = int.TryParse(input, out value);
+        if (success != true)
+        {
+            Console.WriteLine("Please enter a valid number");
+        }
+        else if (value > 10000)
+        {
+            Console.WriteLine("Please enter a number under 10000");
+        }
+    }
+    return value;
+}
+
+int InputMax(bool success, int MIN)
+{
+    string input = "";
+    int value = 0;
+
+    // Ensure conditions of correct user input
+    while (success != true && value < 10000)
+    {
+        Console.WriteLine("Enter the maximum value: ");
+        input = (Console.ReadLine());
+        success = int.TryParse(input, out value);
+        if (success != true)
+        {
+            Console.WriteLine("Please enter a valid number");
+        }
+        else if (value <= MIN && value > 1000000)
+        {
+            Console.WriteLine("Please enter a number higher than the minimum and less than 1,000,000");
+        }
+    }
+    return value;
+}
+
+
+
+int[] CreateBingoBalls(int MIN, int MAX)
+{
+    int[] BingoBalls = new int[MAX - MIN + 1]
+    for (int i = 0; i <= MAX)
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+#### JP CLI Bingo Machine
+
+## How to use?
+TBA
+
+## Why?
+I decided to create this CLI application due to wanting an app to use when playing Japanese Bingo to draw numbers. I figured I may as well create it with C# as that is the language I am comitted to learning at the moment so I thought it would be a neat little project. I am also trying my best to apply clean and scalable code to the best of my ability.
+
+
