315b4b386533c657e837e07c49f2a640412f44cb
David Murray <dcmurray13@hotmail.com>
2024-12-20 15:14:44 +1030

    Somehow lost my working file so updated it the first version that was supposed to be uploaded

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿string input = "";
 int MIN = 1;
 int MAX = 75;
-bool success = false;
+const int ABSOLUTE_MIN = 1;
+const int ABSOLUTE_MAX = 500;
 
 // Receive input from user for default or custom number range
-ProgramWelcome(success);
+ProgramWelcome();
 
 if (input == "Y" || input == "YES")
 {   
-    MIN = InputMin(success);
-    MAX = InputMax(success, MIN);
+    MIN = InputMin();
+    MAX = InputMax(MIN);
 }
 
 Console.WriteLine($"Range: {MIN} to {MAX}");
@@ -20,12 +21,14 @@ CreateBingoBalls(MIN, MAX);
 
 // Function Definitions
 
-void ProgramWelcome(bool success)
+void ProgramWelcome()
 {
+    bool success = false;
     while (!success)
         {
             Console.WriteLine("Would you like to set a custom range of numbers? (Y/N)");
             input = Console.ReadLine().ToString().ToUpper();
+            // Succesful once user input y/n or yes/no
             if (input == "Y" || input == "YES" || input == "N" || input == "NO")
             {
                 success = true;
@@ -33,49 +36,61 @@ void ProgramWelcome(bool success)
         }
 }
 
-int InputMin(bool success)
+int InputMin()
 {
     string input = "";
     int value = 0;
+    bool success = false;
 
     // Ensure conditions of correct user input
-    while (success != true && value < 10000)
+    while (!success)
     {
         Console.WriteLine("Enter the minimum value: ");
         input = (Console.ReadLine());
+
+        // Ensure valid int 
         success = int.TryParse(input, out value);
+
         if (success != true)
         {
             Console.WriteLine("Please enter a valid number");
         }
-        else if (value > 10000)
+        else if (value > ABSOLUTE_MAX - 1 || value < ABSOLUTE_MIN)
         {
-            Console.WriteLine("Please enter a number under 10000");
+            success = false;
+            Console.WriteLine($"Please enter a number between {ABSOLUTE_MIN} and {ABSOLUTE_MAX - 1}");
         }
     }
+
     return value;
 }
 
-int InputMax(bool success, int MIN)
+int InputMax(int MIN)
 {
     string input = "";
     int value = 0;
+    bool success = false;
 
     // Ensure conditions of correct user input
-    while (success != true && value < 10000)
+    while (!success)
     {
         Console.WriteLine("Enter the maximum value: ");
         input = (Console.ReadLine());
+
+        // Ensure valid int
         success = int.TryParse(input, out value);
+        
         if (success != true)
         {
             Console.WriteLine("Please enter a valid number");
         }
-        else if (value <= MIN && value > 1000000)
+        else if (value <= MIN || value > ABSOLUTE_MAX)
         {
-            Console.WriteLine("Please enter a number higher than the minimum and less than 1,000,000");
+            success = false;
+            Console.WriteLine($"Please enter a number between {MIN} and {ABSOLUTE_MAX}");
         }
     }
+
     return value;
 }
 
@@ -83,6 +98,13 @@ int InputMax(bool success, int MIN)
 
 int[] CreateBingoBalls(int MIN, int MAX)
 {
-    int[] BingoBalls = new int[MAX - MIN + 1]
-    for (int i = 0; i <= MAX)
+    int[] BingoBalls = new int[MAX - MIN + 1];
+
+    for (int i = 0; i < MAX; i++)
+    {
+        BingoBalls[i] = MIN;
+        MIN++;
+    }
+
+    return BingoBalls;
 }
